2ab3c73a0e14edfac54a248abff22f881f548b83
Dhruv Upadhyaya <2793dhruv@gmail.com>
2021-04-05 23:47:37 +0530

    Project Tip Calculator

---
+++ Project_1.py
@@ -0,0 +1,7 @@
+age = int(input("Enter your age\n"))
+age_90 = 90 - age
+days = 365 * age_90
+weeks = 52 * age_90
+months = 12 * age_90
+
+print(f"You have {days} days, {weeks} weeks and {months} months")
